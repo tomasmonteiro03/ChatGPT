6efa81dbb25029f44d70a0a4fce55c34aae9e28e
tomasmonteiro03 <76120194+tomasmonteiro03@users.noreply.github.com>
2023-03-22 14:51:26 +0000

    first commit

---
+++ src/main/java/pt/tomasmonteiro/chatgpt/Main.java
@@ -0,0 +1,12 @@
+package pt.tomasmonteiro.chatgpt;
+
+public class Main {
+
+	private static final String TOKEN = "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";
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
